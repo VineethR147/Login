0d799b48fe2d4f02b0a2687645a44740e0d949d4
VineethR147 <vineeth.181ee254@nitk.edu.in>
2019-06-22 08:43:11 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/login/SignUpActivity.java
@@ -0,0 +1,91 @@
+package com.example.login;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ProgressBar progressBar;
+    EditText editTextEmail, editTextPassword;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        mAuth=FirebaseAuth.getInstance();
+        findViewById(R.id.buttonSignUp).setOnClickListener(this);
+        progressBar = (ProgressBar) findViewById(R.id.progressbar);
+    }
+
+    private void registerUser() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        if (email.isEmpty()) {
+            editTextEmail.setError("Email Required");
+            editTextEmail.requestFocus();
+            return;
+
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Enter valid email");
+            editTextEmail.requestFocus();
+            return;
+
+        }
+        if (password.length() < 6) {
+            editTextPassword.setError("Minimum password is 6");
+            editTextPassword.requestFocus();
+            return;
+        }
+        if (password.isEmpty()) {
+            editTextPassword.setError("Password Required");
+            editTextPassword.requestFocus();
+            return;
+
+        }
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(email, password) .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressBar.setVisibility(View.GONE);
+
+               if(task.isSuccessful()){
+                   Toast.makeText(getApplicationContext(), "User Registration Success",Toast.LENGTH_SHORT).show();
+               }
+               else {
+                   Toast.makeText(getApplicationContext(),"Error ",Toast.LENGTH_SHORT).show();
+               }
+            }
+        });
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonSignUp:
+                registerUser();
+                break;
+            case R.id.textViewLogin:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+
+        }
+    }
+}
